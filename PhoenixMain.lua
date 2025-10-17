e6673d3cbddedf6640d240ec63b724aabe4e1aee
kongwot <dailamtran59@gmail.com>
2025-10-17 15:13:11 +0700

    Update PhoenixMain.lua

--- PhoenixMain.lua
+++ PhoenixMain.lua
@@ -1,1 +1,20 @@
+-- nếu dùng ModuleScript: local modules = script.Parent.modules
+local CONFIG = require(script.modules.CONFIG)
+local UTILS = require(script.modules.UTILS)
+local UI = require(script.modules.UI)
+local ESP = require(script.modules.ESP)
+local AIM = require(script.modules.AIMBOT)
+local MOV = require(script.modules.MOVEMENT)
 
+local ScreenGui, Pages = UI.createGui()
+ScreenGui.Parent = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui")
+
+-- ví dụ đăng ký RenderStepped
+local RunService = game:GetService("RunService")
+RunService.RenderStepped:Connect(function(dt)
+    -- ESP update
+    ESP.updateAll()
+    -- Movement update
+    MOV.updateWalkspeed()
+    -- Aimbot active check ...
+end)
