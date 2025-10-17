e4698d10017bde56f0d095ca6182e8c065a93ec3
kongwot <dailamtran59@gmail.com>
2025-10-17 15:12:45 +0700

    Update ESP.lua

--- modules/ESP.lua
+++ modules/ESP.lua
@@ -1,1 +1,28 @@
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+local CONFIG = require(script.Parent.CONFIG)
+local UTILS = require(script.Parent.UTILS)
+
+local M = {}
+M.enemyHighlights = {}
+M.enemyBeams = {}
+
+function M.setupEnemyESP(player)
+    -- copy logic từ script chính: tạo Highlight, set FillColor... tương tự
+end
+
+function M.ensureBeamBetween(localHRP, enemyHRP, id)
+    -- tạo Beam / Attachment
+end
+
+function M.removeBeamForPlayer(id)
+    -- dọn beam
+end
+
+-- optional: export update loop helper
+function M.updateAll()
+    -- gọi setupEnemyESP cho mọi player, manage beams theo Config
+end
+
+return M
 
