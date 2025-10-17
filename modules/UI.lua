503c1001e3831213186e3cc26ad451e338903265
kongwot <dailamtran59@gmail.com>
2025-10-17 15:58:34 +0700

    Delete modules directory

--- modules/UI.lua
+++
@@ -1,8 +0,0 @@
-local UI = {}
-
-function UI.createGui(Config)
-    -- Toàn bộ code phần GUI bạn đã viết trong PhoenixMain.lua
-    -- (đoạn từ “local function createGui()” đến “return ScreenGui, pages”)
-end
-
-return UI
