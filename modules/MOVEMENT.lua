c61cf6e7fd0df6d12f2fcae8a3d84f0a072ecb11
kongwot <dailamtran59@gmail.com>
2025-10-17 15:46:29 +0700

    Add MOVEMENT module with updateWalkspeed and flyHandler

--- modules/MOVEMENT.lua
+++ modules/MOVEMENT.lua
@@ -1,1 +1,11 @@
+local MOVEMENT = {}
 
+function MOVEMENT.updateWalkspeed(Config)
+    -- phần updateWalkspeed()
+end
+
+function MOVEMENT.flyHandler(Config)
+    -- phần Fly control và RenderStepped
+end
+
+return MOVEMENT
